2178812b8f941a46c84135e860a937b42d29c453
duero125 <duero@wp.pl>
2020-03-26 19:11:54 +0100

    Added Comments

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
 
 class Comment(models.Model):
@@ -33,3 +36,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+
+    
